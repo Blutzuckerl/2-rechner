ed6d67f01ca59fa0429246a3c1fcb80116d599b8
Blutzuckerl <266673065+Blutzuckerl@users.noreply.github.com>
2026-03-23 21:17:49 +0100

    Initial commit

---
+++ Form1.Designer.cs
@@ -0,0 +1,279 @@
+﻿
+namespace _2_RECHNER
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Erforderliche Designervariable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Verwendete Ressourcen bereinigen.
+        /// </summary>
+        /// <param name="disposing">True, wenn verwaltete Ressourcen gelöscht werden sollen; andernfalls False.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Vom Windows Form-Designer generierter Code
+
+        /// <summary>
+        /// Erforderliche Methode für die Designerunterstützung.
+        /// Der Inhalt der Methode darf nicht mit dem Code-Editor geändert werden.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.textBox1 = new System.Windows.Forms.TextBox();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.textBox3 = new System.Windows.Forms.TextBox();
+            this.radioButton1 = new System.Windows.Forms.RadioButton();
+            this.radioButton2 = new System.Windows.Forms.RadioButton();
+            this.radioButton3 = new System.Windows.Forms.RadioButton();
+            this.radioButton4 = new System.Windows.Forms.RadioButton();
+            this.radioButton5 = new System.Windows.Forms.RadioButton();
+            this.radioButton6 = new System.Windows.Forms.RadioButton();
+            this.radioButton7 = new System.Windows.Forms.RadioButton();
+            this.radioButton8 = new System.Windows.Forms.RadioButton();
+            this.label1 = new System.Windows.Forms.Label();
+            this.radioButton9 = new System.Windows.Forms.RadioButton();
+            this.radioButton10 = new System.Windows.Forms.RadioButton();
+            this.radioButton11 = new System.Windows.Forms.RadioButton();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.BackColor = System.Drawing.Color.CornflowerBlue;
+            this.button1.Cursor = System.Windows.Forms.Cursors.Help;
+            this.button1.Font = new System.Drawing.Font("Microsoft Sans Serif", 79.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.button1.Location = new System.Drawing.Point(562, 9);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 359);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "=";
+            this.button1.UseVisualStyleBackColor = false;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // textBox1
+            // 
+            this.textBox1.Location = new System.Drawing.Point(12, 106);
+            this.textBox1.Multiline = true;
+            this.textBox1.Name = "textBox1";
+            this.textBox1.Size = new System.Drawing.Size(165, 73);
+            this.textBox1.TabIndex = 1;
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(197, 106);
+            this.textBox2.Multiline = true;
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(166, 73);
+            this.textBox2.TabIndex = 2;
+            // 
+            // textBox3
+            // 
+            this.textBox3.BackColor = System.Drawing.SystemColors.Window;
+            this.textBox3.Cursor = System.Windows.Forms.Cursors.No;
+            this.textBox3.Location = new System.Drawing.Point(761, 106);
+            this.textBox3.Multiline = true;
+            this.textBox3.Name = "textBox3";
+            this.textBox3.Size = new System.Drawing.Size(171, 73);
+            this.textBox3.TabIndex = 3;
+            // 
+            // radioButton1
+            // 
+            this.radioButton1.AutoSize = true;
+            this.radioButton1.Cursor = System.Windows.Forms.Cursors.Default;
+            this.radioButton1.Location = new System.Drawing.Point(418, 106);
+            this.radioButton1.Name = "radioButton1";
+            this.radioButton1.Size = new System.Drawing.Size(37, 21);
+            this.radioButton1.TabIndex = 4;
+            this.radioButton1.TabStop = true;
+            this.radioButton1.Text = "+";
+            this.radioButton1.UseVisualStyleBackColor = true;
+            // 
+            // radioButton2
+            // 
+            this.radioButton2.AutoSize = true;
+            this.radioButton2.Cursor = System.Windows.Forms.Cursors.Default;
+            this.radioButton2.Location = new System.Drawing.Point(299, 263);
+            this.radioButton2.Name = "radioButton2";
+            this.radioButton2.Size = new System.Drawing.Size(139, 21);
+            this.radioButton2.TabIndex = 5;
+            this.radioButton2.TabStop = true;
+            this.radioButton2.Text = "Volumen Zylinder";
+            this.radioButton2.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            this.radioButton2.UseVisualStyleBackColor = true;
+            // 
+            // radioButton3
+            // 
+            this.radioButton3.AutoSize = true;
+            this.radioButton3.Cursor = System.Windows.Forms.Cursors.Default;
+            this.radioButton3.Location = new System.Drawing.Point(444, 263);
+            this.radioButton3.Name = "radioButton3";
+            this.radioButton3.Size = new System.Drawing.Size(112, 21);
+            this.radioButton3.TabIndex = 6;
+            this.radioButton3.TabStop = true;
+            this.radioButton3.Text = "Fläche KReis";
+            this.radioButton3.UseVisualStyleBackColor = true;
+            this.radioButton3.CheckedChanged += new System.EventHandler(this.radioButton3_CheckedChanged);
+            // 
+            // radioButton4
+            // 
+            this.radioButton4.AutoSize = true;
+            this.radioButton4.Cursor = System.Windows.Forms.Cursors.Default;
+            this.radioButton4.Location = new System.Drawing.Point(455, 220);
+            this.radioButton4.Name = "radioButton4";
+            this.radioButton4.Size = new System.Drawing.Size(101, 21);
+            this.radioButton4.TabIndex = 7;
+            this.radioButton4.TabStop = true;
+            this.radioButton4.Text = "Pythagoras";
+            this.radioButton4.UseVisualStyleBackColor = true;
+            // 
+            // radioButton5
+            // 
+            this.radioButton5.AutoSize = true;
+            this.radioButton5.Cursor = System.Windows.Forms.Cursors.Default;
+            this.radioButton5.Location = new System.Drawing.Point(299, 220);
+            this.radioButton5.Name = "radioButton5";
+            this.radioButton5.Size = new System.Drawing.Size(61, 21);
+            this.radioButton5.TabIndex = 8;
+            this.radioButton5.TabStop = true;
+            this.radioButton5.Text = "MOD";
+            this.radioButton5.UseVisualStyleBackColor = true;
+            // 
+            // radioButton6
+            // 
+            this.radioButton6.AutoSize = true;
+            this.radioButton6.Cursor = System.Windows.Forms.Cursors.Default;
+            this.radioButton6.Location = new System.Drawing.Point(495, 158);
+            this.radioButton6.Name = "radioButton6";
+            this.radioButton6.Size = new System.Drawing.Size(33, 21);
+            this.radioButton6.TabIndex = 9;
+            this.radioButton6.TabStop = true;
+            this.radioButton6.Text = "/";
+            this.radioButton6.UseVisualStyleBackColor = true;
+            // 
+            // radioButton7
+            // 
+            this.radioButton7.AutoSize = true;
+            this.radioButton7.Cursor = System.Windows.Forms.Cursors.Default;
+            this.radioButton7.Location = new System.Drawing.Point(418, 158);
+            this.radioButton7.Name = "radioButton7";
+            this.radioButton7.Size = new System.Drawing.Size(34, 21);
+            this.radioButton7.TabIndex = 10;
+            this.radioButton7.TabStop = true;
+            this.radioButton7.Text = "*";
+            this.radioButton7.UseVisualStyleBackColor = true;
+            // 
+            // radioButton8
+            // 
+            this.radioButton8.AutoSize = true;
+            this.radioButton8.Cursor = System.Windows.Forms.Cursors.Default;
+            this.radioButton8.Location = new System.Drawing.Point(494, 106);
+            this.radioButton8.Name = "radioButton8";
+            this.radioButton8.Size = new System.Drawing.Size(34, 21);
+            this.radioButton8.TabIndex = 11;
+            this.radioButton8.TabStop = true;
+            this.radioButton8.Text = "-";
+            this.radioButton8.UseVisualStyleBackColor = true;
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Font = new System.Drawing.Font("Minecraft", 36F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(0)));
+            this.label1.Location = new System.Drawing.Point(44, 24);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(355, 60);
+            this.label1.TabIndex = 12;
+            this.label1.Text = "RECHNER:";
+            // 
+            // radioButton9
+            // 
+            this.radioButton9.AutoSize = true;
+            this.radioButton9.Location = new System.Drawing.Point(289, 306);
+            this.radioButton9.Name = "radioButton9";
+            this.radioButton9.Size = new System.Drawing.Size(51, 21);
+            this.radioButton9.TabIndex = 13;
+            this.radioButton9.TabStop = true;
+            this.radioButton9.Text = "SIN";
+            this.radioButton9.UseVisualStyleBackColor = true;
+            // 
+            // radioButton10
+            // 
+            this.radioButton10.AutoSize = true;
+            this.radioButton10.Location = new System.Drawing.Point(438, 306);
+            this.radioButton10.Name = "radioButton10";
+            this.radioButton10.Size = new System.Drawing.Size(53, 21);
+            this.radioButton10.TabIndex = 14;
+            this.radioButton10.TabStop = true;
+            this.radioButton10.Text = "Cos";
+            this.radioButton10.UseVisualStyleBackColor = true;
+            // 
+            // radioButton11
+            // 
+            this.radioButton11.AutoSize = true;
+            this.radioButton11.Location = new System.Drawing.Point(346, 347);
+            this.radioButton11.Name = "radioButton11";
+            this.radioButton11.Size = new System.Drawing.Size(106, 21);
+            this.radioButton11.TabIndex = 15;
+            this.radioButton11.TabStop = true;
+            this.radioButton11.Text = "Quersumme";
+            this.radioButton11.UseVisualStyleBackColor = true;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(992, 513);
+            this.Controls.Add(this.radioButton11);
+            this.Controls.Add(this.radioButton10);
+            this.Controls.Add(this.radioButton9);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.radioButton8);
+            this.Controls.Add(this.radioButton7);
+            this.Controls.Add(this.radioButton6);
+            this.Controls.Add(this.radioButton5);
+            this.Controls.Add(this.radioButton4);
+            this.Controls.Add(this.radioButton3);
+            this.Controls.Add(this.radioButton2);
+            this.Controls.Add(this.radioButton1);
+            this.Controls.Add(this.textBox3);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.textBox1);
+            this.Controls.Add(this.button1);
+            this.Cursor = System.Windows.Forms.Cursors.UpArrow;
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.TextBox textBox1;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.TextBox textBox3;
+        private System.Windows.Forms.RadioButton radioButton1;
+        private System.Windows.Forms.RadioButton radioButton2;
+        private System.Windows.Forms.RadioButton radioButton3;
+        private System.Windows.Forms.RadioButton radioButton4;
+        private System.Windows.Forms.RadioButton radioButton5;
+        private System.Windows.Forms.RadioButton radioButton6;
+        private System.Windows.Forms.RadioButton radioButton7;
+        private System.Windows.Forms.RadioButton radioButton8;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.RadioButton radioButton9;
+        private System.Windows.Forms.RadioButton radioButton10;
+        private System.Windows.Forms.RadioButton radioButton11;
+    }
+}
+
---
+++ Form1.cs
@@ -0,0 +1,181 @@
+﻿using System;
+using Microsoft.VisualBasic;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace _2_RECHNER
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+
+        private void radioButton3_CheckedChanged(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            double dblEingabe1;
+            double dblEingabe2;
+            string strEingabe1;
+            string strEingabe2;
+            string strCaption;
+            double Ergebnis;
+            string strText;
+            bool canConvertEingabe1;
+            bool canConvertEingabe2;
+           
+ 
+
+            strCaption = "Zahleneingabe:";
+            strText = "Geben Sie bitte eine Zahl ein:";
+            strEingabe2 = textBox2.Text;
+            strEingabe1 = textBox1.Text;
+            
+            // Variante 1: Beginn der Schleife (Abbruchbedingung am Ende: 1mal ausgeführt)
+            
+
+
+
+                // Überprüfen ob der Eingabe eine Zahl (=numerisch) ist?
+                canConvertEingabe1 = double.TryParse(strEingabe1, out dblEingabe1);
+                canConvertEingabe2 = double.TryParse(strEingabe2, out dblEingabe2);
+                        if (canConvertEingabe2 = double.TryParse(strEingabe2, out dblEingabe2))
+            {
+                dblEingabe1 = System.Convert.ToDouble(strEingabe1);
+            }
+                        else
+            {
+                if (this.radioButton3.Checked == true)
+                {
+
+                }
+                MessageBox.Show("Bitte eine Zahl eingeben!!!", "Falsche Eingabe!!!", MessageBoxButtons.OK,
+                                MessageBoxIcon.Stop, MessageBoxDefaultButton.Button1,
+                                MessageBoxOptions.DefaultDesktopOnly);
+            }
+            canConvertEingabe2 = double.TryParse(strEingabe2, out dblEingabe2);
+            canConvertEingabe1 = double.TryParse(strEingabe1, out dblEingabe1);
+            // Wenn NEIN ==> MsBox mit Hinweis dass kein Zahl eingeben wurde!
+            if (this.radioButton1.Checked == true)
+            {
+                Ergebnis = dblEingabe1 + dblEingabe2;
+                this.textBox3.Text = Ergebnis.ToString();
+
+            }
+            else if (this.radioButton8.Checked == true)
+            {
+                Ergebnis = dblEingabe1 - dblEingabe2;
+                this.textBox3.Text = Ergebnis.ToString();
+            }
+            else if (this.radioButton7.Checked == true)
+            {
+                Ergebnis = dblEingabe2 * dblEingabe2;
+                this.textBox3.Text = Ergebnis.ToString();
+            }
+            else if (this.radioButton6.Checked == true)
+            {
+                if (dblEingabe2 != 0)
+                {
+                    Ergebnis = dblEingabe1 / dblEingabe2;
+                    this.textBox3.Text = Ergebnis.ToString();
+                }
+                else
+                {
+                    MessageBox.Show("Die zweite Zahl darf nicht kleiner als 0 sein.", "Falsche Eingabe!!!", MessageBoxButtons.OK,
+                MessageBoxIcon.Stop, MessageBoxDefaultButton.Button1,
+                MessageBoxOptions.DefaultDesktopOnly);
+                }
+            }
+            else if (this.radioButton5.Checked == true)
+            {
+
+                if (dblEingabe1 != 0 && dblEingabe2 != 0)
+                {
+                    Ergebnis = dblEingabe1 % dblEingabe2;
+                    this.textBox3.Text = Ergebnis.ToString();
+                }
+                else
+                {
+                    MessageBox.Show("Die zweite Zahl und/oder Zahl 1 darf nicht kleiner als 0 sein.", "Falsche Eingabe!!!", MessageBoxButtons.OK,
+                    MessageBoxIcon.Stop, MessageBoxDefaultButton.Button1,
+                    MessageBoxOptions.DefaultDesktopOnly);
+                }
+            }
+            else if (this.radioButton4.Checked == true)
+            {
+                Ergebnis = Math.Sqrt(Math.Pow(dblEingabe1, 2) + Math.Pow(dblEingabe2, 2));
+                this.textBox3.Text = Ergebnis.ToString();
+            }
+
+            else if (this.radioButton3.Checked == true)
+            {
+
+
+              //  MessageBox.Show("bitte 0 bei der 2 zahl eingeben", "Richtge Eingabe!!!", MessageBoxButtons.OK,
+               //  MessageBoxIcon.Information, MessageBoxDefaultButton.Button1,
+               //  MessageBoxOptions.DefaultDesktopOnly);
+
+
+                Ergebnis = Math.Sqrt(Math.Pow(dblEingabe1, 2)) * Math.PI * Math.Sqrt(Math.Pow(dblEingabe2, 0));
+                this.textBox3.Text = Ergebnis.ToString();
+            }
+
+
+            else if (this.radioButton2.Checked == true)
+            {
+
+                Ergebnis = Math.Sqrt(Math.Pow(dblEingabe1, 2)) * Math.PI * dblEingabe2;
+                this.textBox3.Text = Ergebnis.ToString();
+            }
+
+
+            else if (this.radioButton9.Checked == true)
+            {
+                double winkl;
+                winkl = Math.PI * dblEingabe1 / 180.0;
+                Ergebnis = Math.Sin(winkl);
+                this.textBox3.Text = Ergebnis.ToString();
+
+            }
+            else if (this.radioButton10.Checked == true)
+            {
+                double winkl;
+                winkl = Math.PI * dblEingabe1 / 180.0;
+                Ergebnis = Math.Cos(winkl);
+                this.textBox3.Text = Ergebnis.ToString();
+            }
+            else if (this.radioButton11.Checked == true)
+            {
+                int i;  
+                int Ziffer;
+                long quersumme = 0;
+                string qSummeEingabe;
+
+                qSummeEingabe = Math.Floor(dblEingabe1).ToString();
+                //Length is wichtig
+                for ( i = 0; i <= qSummeEingabe.Length-1; i++)
+                {
+
+                    Ziffer = Convert.ToInt32(strEingabe1.Substring(i,1)) ;
+                    quersumme = quersumme + Ziffer; //<-- muss sein weil long in string
+                
+                }
+                 this.textBox3.Text = quersumme.ToString();
+          
+            }
+        }
+       
+        }
+    }
+
